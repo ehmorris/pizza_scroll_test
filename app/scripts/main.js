deeb53f601048522850dc95314cab80332fcb66a
Edwin Morris <edwin@rapgenius.com>
2017-01-28 11:22:09 -0500

    scrub html5 video on scroll

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,38 +1,18 @@
-var player, total_height, window_height, total_duration;
+var video = $('video')[0];
+var video_duration;
+var document_height = $(document).height();
+var window_height = window.innerHeight;
 
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
+video.addEventListener('loadeddata', function() {
+  video_duration = video.duration;
 
-  scroll_seek(player);
-}
-
-function onPlayerReady(event) {
-  console.log('ready');
-  event.target.playVideo();
-  total_duration = player.getDuration()
-}
-
-function onPlayerStateChange(event) {
-  console.log(event.data);
-}
-
-function getDimensions() {
-  total_height = $(document).height();
-  window_height = window.innerHeight;
-}
-
-function scroll_seek(player) {
   window.addEventListener('scroll', function(event) {
-    getDimensions();
-    var percent_scrolled = window.scrollY / (total_height - window_height);
+    var percent_scrolled = window.scrollY / (document_height - window_height);
 
-    var seek_in_seconds = total_duration * percent_scrolled;
+    var seek_in_seconds = video_duration * percent_scrolled;
 
-    player.seekTo(seek_in_seconds, true);
+    video.currentTime = seek_in_seconds;
+
+    video.play();
   });
-};
+});
