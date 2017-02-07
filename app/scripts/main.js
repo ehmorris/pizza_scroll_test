74a0cf6211811a2066806898fa8531ad87511709
Edwin Morris <edwin@rapgenius.com>
2017-02-07 18:58:06 -0500

    detect container height

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,13 @@
 let iframe = document.querySelector('iframe');
 let player = new Vimeo.Player(iframe);
 let video_duration = null;
+let container_height = $(document).height();
 
 let clip_number = (n) => {
   return Math.min(Math.max(n, .01), .99);
 }
 
 let seek_player = () => {
-  let container_height = 1856;
   let element_scroll_distance = window.scrollY;
   let percent_scrolled =
     clip_number(element_scroll_distance / container_height);
