b8d9f02f2537584f5915c81ed88f9ff0ba808d1e
Edwin <edwin.h.morris@gmail.com>
2017-02-07 20:19:22 -0500

    Multiple articles, multiple videos
    As each article enters the viewport, its video is loaded into the background YouTube player. Articles are separated by full viewport height title cards.
    
    Known issue:
    - If the user loads the page with an anchor tag, or the browser for other reasons scrolls them down automatically, many articles can be activated by InView in quick succession. Since the YouTube initializer isn't instantaneous, the video id we pass to the initializer is sometimes not the correct video for the article eventually shown, as several other article activations / scroll events could occur while we wait for the initialization callback.

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,49 +1,127 @@
-var youtube_player, scrollable_height, video_duration;
-
-let scroll_seek = (youtube_player) => {
-  window.addEventListener('scroll', function() {
-    var percent_scrolled = window.scrollY / scrollable_height;
-
-    var seek_in_seconds = video_duration * percent_scrolled;
-
-    youtube_player.seekTo(seek_in_seconds, true);
-  });
+const player_settings = {
+  modestbranding: 1,
+  rel: 0,
+  showinfo: 0,
+  loop: 0,
+  playsinline: 0,
+  controls: 0,
+  autoplay: 0,
 };
 
-window.onYouTubeIframeAPIReady = () => {
-  youtube_player = new YT.Player('youtube_player', {
-    events: {
-      'onReady': on_ready,
-      'onStateChange': on_state_change
-    }
+let video_list = [
+  {
+    id: 'rZojgVucBTA',
+    duration: 108,
+    $container: null,
+    container_height: null,
+    container_top_offset: null
+  },
+  {
+    id: 'YFO1Wrkk6Bs',
+    duration: 1663,
+    $container: null,
+    container_height: null,
+    container_top_offset: null
+  },
+  {
+    id: 'zg9FIwCmx5w',
+    duration: 143,
+    $container: null,
+    container_height: null,
+    container_top_offset: null
+  }
+];
+
+let player = null;
+let player_is_initialized, player_being_initialized = false;
+let video = null;
+let new_video_loaded = false;
+let scrolling_stopped = null;
+
+let clip_number = (n) => {
+  return Math.min(Math.max(n, .01), .99);
+}
+
+let activate_focus_mode = () => {
+  $('body').addClass('video_in_focus');
+}
+
+let deactivate_focus_mode = () => {
+  $('body').removeClass('video_in_focus');
+}
+
+let new_youtube_player = (element_id, video_id, callback) => {
+  return new YT.Player(element_id, {
+    videoId: video_id,
+    playerVars: player_settings,
+    events: { 'onReady': callback }
   });
 }
 
-let on_ready = () => {
-  video_duration = youtube_player.getDuration();
+let set_video_values = () => {
+  console.group('video_values');
+  if (!video.container_height) {
+    console.log(`video.container_height = ${video.$container.height()}`);
+    video.container_height = video.$container.height();
+  }
 
-  let get_browser_dimensions = function() {
-    let document_height = $(document).height();
-    let viewport_height = window.innerHeight;
-    scrollable_height = document_height - viewport_height;
-  }();
+  if (!video.container_top_offset) {
+    console.log(`video.container_top_offset = ${video.$container.offset().top}`);
+    video.container_top_offset = video.$container.offset().top;
+  }
 
-  scroll_seek(youtube_player);
+  console.groupEnd();
 }
 
-let on_state_change = (state) => {
-  if (state.data === -1) {
-    $('.video-cover').removeClass('video-cover--black');
+let seek_player = () => {
+  let element_scroll_distance =
+    window.scrollY - video.container_top_offset;
+  let percent_scrolled =
+    clip_number(element_scroll_distance / video.container_height);
+
+  player.seekTo(video.duration * percent_scrolled, true);
+
+  activate_focus_mode();
+  if (scrolling_stopped) window.clearTimeout(scrolling_stopped);
+  scrolling_stopped = window.setTimeout(deactivate_focus_mode, 300);
+}
+
+let activate_article_video = ($article) => {
+  let article_index = $('.article').index($article);
+  video = video_list[article_index];
+
+  console.log(`activate article ${article_index}, video ${video.id}`);
+
+  if (!video.$container) {
+    video.$container = $article;
+  }
+
+  if (!player_is_initialized && !player_being_initialized) {
+    player_being_initialized = true;
+    console.log(`player being initialized with ${video.id}`);
+    player = new_youtube_player('video_element', video.id, () => {
+      console.log('player done initializing');
+      player_is_initialized = true;
+      player_being_initialized = false;
+      set_video_values();
+      window.addEventListener('scroll', seek_player);
+    });
+  } else if (player_is_initialized && !player_being_initialized) {
+    console.log(`player.loadVideoById(${video.id})`);
+    player.loadVideoById(video.id);
+    set_video_values();
   }
 }
 
-let activate_video_focus_mode = () => {
-  $('body').addClass('video_in_focus');
+window.onYouTubeIframeAPIReady = () => {
+  $('.article').each(function() {
+    let $article = $(this);
+
+    new Waypoint.Inview({
+      element: $(this)[0],
+      enter: () => {
+        activate_article_video($article);
+      }
+    });
+  });
 };
-
-let deactivate_video_focus_mode = () => {
-  $('body').removeClass('video_in_focus article_in_focus');
-}
-
-$('body').on('touchstart', activate_video_focus_mode);
-$('body').on('touchend', deactivate_video_focus_mode);
